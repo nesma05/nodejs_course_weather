07617833df4792cbbb98834223a66411f16acc8b
nesma <chalinfy@gmail.com>
2020-09-29 17:51:38 +0000

    removed unessessary console.log

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log("this is from public js");
-
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
 const messageOne = document.querySelector("#message-1");
